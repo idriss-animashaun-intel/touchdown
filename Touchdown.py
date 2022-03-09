086b491e632ec76d9c07490ac76111129c47278e
hcollins <harry.collins@intel.com>
2022-03-09 15:22:50 +0000

    Update to make initial run faster and work with toolshed

--- Touchdown.py
+++ Touchdown.py
@@ -29,40 +29,51 @@ def upgrade():
     installation()
 
 
-### Is touchdown already installed? If yes get file size to compare for upgrade
-if os.path.isfile(touchdown_file):
-    local_file_size = int(os.path.getsize(touchdown_rev))
-    # print(local_file_size)
-    ### Check if update needed:
-    f = urllib.request.urlopen("https://gitlab.devtools.intel.com/ianimash/touchdown/-/raw/updates/Rev.txt") # points to the exe file for size
-    i = f.info()
-    web_file_size = int(i["Content-Length"])
-    # print(web_file_size)
+def main(autoinstall=0):
+    ### Is touchdown already installed? If yes get file size to compare for upgrade
+    if os.path.isfile(touchdown_file):
+        local_file_size = int(os.path.getsize(touchdown_rev))
+        # print(local_file_size)
+        ### Check if update needed:
+        f = urllib.request.urlopen("https://gitlab.devtools.intel.com/ianimash/touchdown/-/raw/updates/Rev.txt") # points to the exe file for size
+        i = f.info()
+        web_file_size = int(i["Content-Length"])
+        # print(web_file_size)
+        if local_file_size != web_file_size:# upgrade available
+            if autoinstall:
+                print("*** New upgrade available! Upgrading now *** ")
+                upgrade()
+            else:
+                updt = input("*** New upgrade available! enter <y> to upgrade now, other key to skip upgrade *** ")
+                if updt == "y": # proceed to upgrade
+                    upgrade()
+                elif updt == "Y":
+                    upgrade()
+    ### touchdown wasn't installed, so we download and install it here                
+    else:
+        if autoinstall:
+            print("*** Installing Touchdown for the first time ***")
+            installation()
+        else:
+            install = input("Welcome to touchdown! If you enter <y> Touchdown will be downloaded in the same folder where this file is.\nAfter the installation, this same file you are running now (\"touchdown.exe\") will the one to use to open touchdown :)\nEnter any other key to skip the download\n -->")
+            if install == "y":
+                installation()
+            elif install == "Y":
+                installation()
+    print('Ready')
+    ### We open the real application:
+    try:
+        Popen(touchdown_file)
+        print("*** Opening Touchdown ***")
+        if not autoinstall:
+            time.sleep(20)
+    except:
+        print('Failed to open application, Please open manually in subfolder')
+        pass
 
 
-    if local_file_size != web_file_size:# upgrade available
-        updt = input("*** New upgrade available! enter <y> to upgrade now, other key to skip upgrade *** ")
-        if updt == "y": # proceed to upgrade
-            upgrade()
-        elif updt == "Y":
-            upgrade()
+def main_with_autoinstall():
+    main(autoinstall=1)
 
-### touchdown wasn't installed, so we download and install it here                
-else:
-    install = input("Welcome to touchdown! If you enter <y> Touchdown will be downloaded in the same folder where this file is.\nAfter the installation, this same file you are running now (\"touchdown.exe\") will the one to use to open touchdown :)\nEnter any other key to skip the download\n -->")
-    if install == "y":
-        installation()
-    elif install == "Y":
-        installation()
-
-print('Ready')
-
-
-### We open the real application:
-try:
-    Popen(touchdown_file)
-    print("*** Opening Touchdown ***")
-    time.sleep(20)
-except:
-    print('Failed to open application, Please open manually in subfolder')
-    pass
+if __name__ == "__main__":
+    main()
--- cbcli_locator.bat
+++ cbcli_locator.bat
@@ -1,4 +1,4 @@
 set "curpath="%cd%""
-cd \
+cd %USERPROFILE%\CrystalBall\
 dir /s /b cbcli.exe > %curpath%\cbcli_loc.txt
 cd %curpath%
